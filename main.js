8eefd5c3ad6b42dfece78cef3de8691bdc4f2ec4
safa-hamrouni <hamrouni.safaa@gmail.com>
2020-05-14 00:19:51 +0100

    -

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-# First-Karla-Alan-Project
+
+$("main").css({"background-image":"url('https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')", "background-size":"cover"});
+$("body").css("background-image", "url('https://previews.123rf.com/images/drekhann/drekhann1809/drekhann180900241/109238524-funny-doodle-monsters-seamless-pattern-for-prints-designs-and-coloring-books-vector-illustration.jpg')");
+$("header").css({"background-color":"black", "color":"white"})
