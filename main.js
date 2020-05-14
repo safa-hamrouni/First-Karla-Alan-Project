acb913def79df04bb0eb3030fe4ed388a7c3e288
med-alaa-rb <alaa.rabai@gmail.com>
2020-05-14 02:46:26 +0100

    👌

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 $("main").css({"background-image":"url('https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')", "background-size":"cover"});
 $("body").css("background-image", "url('https://previews.123rf.com/images/drekhann/drekhann1809/drekhann180900241/109238524-funny-doodle-monsters-seamless-pattern-for-prints-designs-and-coloring-books-vector-illustration.jpg')");
 $("header").css({"background-color":"black", "color":"white"})
+//var answers = $("#question").append("<li class = "answer">text</li>");
+//function addList(answers){
+//	for (var i = 0 ; i <= 3 ; i++){
+//		return answers;
+//	}
+//}
