3d8091e4bc19585393371ff0803fa360fc5133af
safa-hamrouni <hamrouni.safaa@gmail.com>
2020-05-15 18:03:34 +0100

    -

--- main.js
+++ main.js
@@ -9,24 +9,39 @@ var array = [
     {question:'Coprastastaphobia is the fear of what?', answer:['JavaScript','Spiders', 'Constipation']},
     {question: 'It\'s illegal in Texas to put what on your neighbour\’s Cow?', answer:['A bell','Graffiti', 'Your Picture 🤭']},
     {question:' Who entered a contest to find his own look-alike and came 3rd?', answer:['Mr.Bean','Charlie Chaplin', 'Assia(from Nouba)🙄']},
-    {question: 'French artist Aquabouse paints cows with what material?', answer1:['Cow-Blood','Cow-Milk','Cow-DunG']},
+    {question: 'French artist Aquabouse paints cows with what material?', answer:['Cow-Blood','Cow-Milk','Cow-DunG']},
     {question:'What were the first ice hockey pucks made out of?', answer:['Wood','Frozen Cow-DunG','Vulcanized Rubber']},
-    {question:'In which country are there six villages called Silly and 12 called Billy?', answer1:['Grenada','France','Mangolia']},
+    {question:'In which country are there six villages called Silly and 12 called Billy?', answer:['Grenada','France','Mangolia']},
     {question:'In the Caribbean, what sort of animal is the mountain chicken?', answer:['Frogs','Birds','Parrots']},
     {question:'Who invented the word vomit?', answer:['Karla Ben Moncef','Khawarzmi','William Shakespear']},
     {question:'The leader of which country has his face on all the joghurt pots?', answer:['Tanzania','Azarbidjan','Turkmenistan']},
     {question:'What is the cross between a donkey and a zebra known as?', answer:['ZeeDonk','DonKra','CapyBara']}
 			]
- var didYouKnow = ['did you really think we meant kanoun.... it\'s 9anoun', 'Yup it\'s pink.. Don\'t be sexist !','It’s natural to be afraid of JavaScript but This one was a real phobia !', 
+ var didYouKnow = ['Did you really think we meant kanoun.... it\'s 9anoun', 'It\'s pink.. Can you believe it 🤦‍♀️ !','It’s natural to be afraid of JavaScript but This one was a real phobia !', 
  	'I really hope you didn\'t think you can put your picture on a cow',
 	'If you didn\'t see Nouba.. you wouldn\'t know ! 😅', 'You don\'t know what cow-dung is ??? Look it up (link)', 'Now you should know what cow dung is !', 'I thought France had more vocabulary than just that !',
 	'Who would ever think that Chickens could be frogs ! Magic..', 'Yup! invented Romeo and Juliet, Hamlet AND vomit .. ', 'I think Trump is expected to do it next !','You probably guessed it right away !'];
 
- var correctAnswer = ['harp','Pink','Constipation','Graffiti','Charlie Chaplin','Cow-DunG','frozen Cow-DunG','France','Frogs','William Shakespear','Turkmenistan','ZeeDonk']
+ var correctAnswer = ['Harp','Pink','Constipation','Graffiti','Charlie Chaplin','Cow-DunG','Frozen Cow-DunG','France','Frogs','William Shakespear','Turkmenistan','ZeeDonk']
+
 
 function display(){
 	var i = 0;
 	return {
+		start: function(){
+			$(".deco").hide()
+			 $("#start").on("click", function(){
+			 	setTimeout(function(){$(".deco").show()
+			 	$("#start").hide()
+			 },1000)
+			})
+		},
+		end : function(arr){
+			if(i >12){
+				$(".deco").hide()
+
+			}
+		},
 		up: function(){
 			i++
 		},
@@ -40,34 +55,40 @@ function display(){
 			$(' ul > .answer3').text(arr[i].answer[2])
 		},
 		correct: function(didYouKnow, correctAnswer){
-			$('.answer1, .answer2, .answer3').on("click", function(){
-               var test = $(this)
-	 		if(test.text() === correctAnswer[i]){
-	 			test.css('background-color', 'green');
-	 			$('#reveal').text(didYouKnow[i]);
+				$('.ans').on("click", function(){
+					var e = $(this)
+					if(e.text() === correctAnswer[i]){
+			 			e.css('background-color', 'green');
+			 			
+					}
+					 else {
+					 	e.css("background-color", "red")
+					 }
+					$('#reveal').text(didYouKnow[i]);
+				})
+		},
+		default : function(){
+				$('li').css("background-color", "pink")
+				$('#reveal').text('')
 		}
-		  alert('sdsdf')
-	   })
-      }
-	};
+	}
 }
+
+var start = display()
+start.start()
 
 var nextQuestion = display()
 nextQuestion.questionAnswer(array)
-
-$('#confirm').on('click', function(){
-    nextQuestion.correct(didYouKnow,correctAnswer)
-	nextQuestion.up()
-	nextQuestion.questionAnswer(array)
-	
+$('.ans').hover(function(){
+nextQuestion.correct(didYouKnow,correctAnswer)
 })
 
 
- // var x = display()
- // x.correct(didYouKnow, correctAnswer)	
- // console.log(x)
- // x.up()
- // console.log(x)
-
-
+$('#confirm').on('click', function(){
+	nextQuestion.default()
+	nextQuestion.up()
+	nextQuestion.questionAnswer(array)
+})
  
+var end = display()
+end.end(array)
