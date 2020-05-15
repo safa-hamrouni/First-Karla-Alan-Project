decedc00ea5a7c0a8be8dcb70e0812b6e26f6fb7
safa-hamrouni <hamrouni.safaa@gmail.com>
2020-05-15 22:19:59 +0100

    -

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 $("main").css({"background-image":"url('https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')", "background-size":"cover"});
 $("body").css("background-image", "url('https://previews.123rf.com/images/drekhann/drekhann1809/drekhann180900241/109238524-funny-doodle-monsters-seamless-pattern-for-prints-designs-and-coloring-books-vector-illustration.jpg')");
-$("header").css({"background-color":"black", "color":"white"})
+$("header").css("background-image","url('https://cdn.pixabay.com/photo/2017/02/12/12/42/wall-2059909_960_720.png')")
 
 var array = [
     {question: 'Which instrument has forty-seven strings and seven pedals?', answer:['Piano','Kanoon','Harp']},
